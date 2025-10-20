a329b1e56618cd68dfa1e93b5f7c2f149c49fd85
kinjoyuji <kinjo.yuji@techmagic.co.jp>
2025-10-20 18:46:25 +0900

    Update parameter file reference from GS5-1.yaml to GS5.yaml in launch files

--- launch/ydlidar_launch.py
+++ launch/ydlidar_launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
 
     params_declare = DeclareLaunchArgument('params_file',
                                            default_value=os.path.join(
-                                               share_dir, 'params', 'GS5-1.yaml'),
+                                               share_dir, 'params', 'GS5.yaml'),
                                            description='FPath to the ROS2 parameters file to use.')
 
     driver_node = LifecycleNode(package='ydlidar_ros2_driver',
